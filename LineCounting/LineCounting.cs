29daef1858d565a2d907e16b7c42caf7f2bddee7
Vlad264 <vlad-498@yandex.ru>
2019-04-29 19:56:12 +0700

    Create base and Main

---
+++ LineCounting/LineCounting.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace LineCounting
+{
+    class LineCounting
+    {
+        private readonly string format;
+
+        public LineCounting(string format)
+        {
+            this.format = format;
+        }
+
+        public int Count()
+        {
+            return 0;
+        }
+    }
+}
--- LineCounting/Program.cs
+++ LineCounting/Program.cs
@@ -6,7 +6,15 @@ namespace LineCounting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Incorrect number pf arguments");
+                Console.WriteLine("Usage: dotnet run FILE_FORMAT");
+                return;
+            }
+            var lineCounting = new LineCounting(args[0]);
+            var result = lineCounting.Count();
+            Console.WriteLine("Result: {0} lines", result);
         }
     }
 }
