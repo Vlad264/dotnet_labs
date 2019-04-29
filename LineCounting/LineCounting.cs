599b7f25a5e3923ccfe87d08321045bed6343d65
Vlad264 <vlad-498@yandex.ru>
2019-04-29 20:49:06 +0700

    Finish LineCounting

--- LineCounting/LineCounting.cs
+++ LineCounting/LineCounting.cs
@@ -1,10 +1,16 @@
 using System;
+using System.IO;
 
 namespace LineCounting
 {
     class LineCounting
     {
+        private readonly string singleComment = "//";
+        private readonly string multiCommentStart = "/*";
+        private readonly string multiCommentEnd = "*/";
+
         private readonly string format;
+        private int result;
 
         public LineCounting(string format)
         {
@@ -13,7 +19,56 @@ namespace LineCounting
 
         public int Count()
         {
-            return 0;
+            result = 0;
+            SearchDir(new System.IO.DirectoryInfo(System.IO.Directory.GetCurrentDirectory()));
+            return result;
+        }
+
+        private void SearchDir(System.IO.DirectoryInfo di)
+        {
+            System.IO.FileInfo[] subFiles = di.GetFiles(format);
+            foreach (var file in subFiles)
+            {
+                CountLines(file);
+            }
+
+            System.IO.DirectoryInfo[] subDir = di.GetDirectories();
+            foreach (var dir in subDir)
+            {
+                SearchDir(dir);
+            }
+        }
+
+        private void CountLines(System.IO.FileInfo file)
+        {
+            int count = 0;
+            string str;
+            using (StreamReader reader = file.OpenText())
+            {
+                while ((str = reader.ReadLine()) != null)
+                {
+                    var trim = str.Trim();
+                    if (trim == "" || trim.StartsWith(singleComment)) {
+                        continue;
+                    }
+                    if (trim.Contains(multiCommentStart))
+                    {
+                        if (trim.IndexOf(multiCommentStart) != 0)
+                        {
+                            ++count;
+                        }
+                        while ((str = reader.ReadLine()) != null && !str.Trim().Contains(multiCommentEnd));
+                        if (str.Trim().IndexOf(multiCommentEnd) != 0)
+                        {
+                            ++count;
+                        }
+                        continue;
+                    }
+                    ++count;
+                }
+            }
+            Console.WriteLine("In file {0}: {1} lines", file.Name, count);
+            result += count;
         }
     }
 }
--- LineCounting/Program.cs
+++ LineCounting/Program.cs
@@ -12,6 +12,7 @@ namespace LineCounting
                 Console.WriteLine("Usage: dotnet run FILE_FORMAT");
                 return;
             }
+            //Console.WriteLine("Result: {0} lines", args[0]);
             var lineCounting = new LineCounting(args[0]);
             var result = lineCounting.Count();
             Console.WriteLine("Result: {0} lines", result);
