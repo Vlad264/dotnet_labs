3a796fd88e4c29cfbf6159c83719d1e66425663f
Vlad264 <vlad-498@yandex.ru>
2019-05-25 21:59:29 +0700

    Add VkNet and write authorize

---
+++ SocialNetwork/SocialNetwork/VKHandler.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.DependencyInjection;
+using VkNet;
+using VkNet.AudioBypassService.Extensions;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkNet.Utils;
+
+namespace SocialNetwork
+{
+    public class VKHandler
+    {
+        private const int AppId = 5288513;
+        private readonly VkApi vk;
+        private VkCollection<User> friends;
+
+        VKHandler(string login, string password)
+        {
+            var service = new ServiceCollection();
+            service.AddAudioBypass();
+            
+            vk = new VkApi(service);
+            vk.Authorize(new ApiAuthParams
+            {
+                ApplicationId = AppId,
+                Login = login,
+                Password = password,
+                Settings = Settings.Friends
+            });
+        }
+    }
+}
