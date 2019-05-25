5463e21e0466b953ae85d39ce22b7e57ba428e64
Vlad264 <vlad-498@yandex.ru>
2019-05-25 19:44:59 +0700

    Create simple controller and cshtml for index, employee and project

---
+++ EffectiveEmployee/EffectiveEmployee/Controllers/MainController.cs
@@ -0,0 +1,52 @@
+using System.Linq;
+using EffectiveEmployee.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EffectiveEmployee.Controllers
+{
+    [Route("/")]
+    public class MainController : Controller
+    {
+        private readonly DataContext context;
+
+        public MainController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [Route("")]
+        public IActionResult Index() => View(context.Employees.ToList());
+
+        [Route("Employee")]
+        public IActionResult Employee(int id)
+        {
+            Employee emp;
+            if (id > 0)
+            {
+                emp = context.Employees.Find(id);
+            }
+            else
+            {
+                emp = new Employee();
+            }
+
+            return View(emp);
+        }
+
+        [Route(("Project"))]
+        public IActionResult Project(int id)
+        {
+            Project project;
+            if (id > 0)
+            {
+                project = context.Projects.Find(id);
+            }
+            else
+            {
+                project = new Project();
+            }
+
+            return View(project);
+        }
+    }
+}
