a4c2ff396d48dcf07645f415cc23dfa3a694ae35
Vlad264 <vlad-498@yandex.ru>
2019-05-25 23:11:53 +0700

    Finish SocialNetwork lab with VkNet Api

--- SocialNetwork/SocialNetwork/Program.cs
+++ SocialNetwork/SocialNetwork/Program.cs
@@ -16,6 +16,7 @@ namespace SocialNetwork
             try
             {
                 VKHandler vkHandler = new VKHandler(login, password);
+                Run(vkHandler);
             }
             catch (VkAuthorizationException e)
             {
@@ -24,6 +25,42 @@ namespace SocialNetwork
             catch (Exception e)
             {
                 Console.WriteLine("Network error: " + e.Message);
+            }
+        }
+
+        private static void Run(VKHandler vkHandler)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter user name to write to this friend");
+                Console.WriteLine("Enter \"friends\" to see friends list");
+                Console.WriteLine("Enter \"exit\" to close program");
+                var str = Console.ReadLine();
+                switch (str)
+                {
+                    case "friends":
+                        vkHandler.WriteFriends();
+                        break;
+                    
+                    case "exit":
+                        Environment.Exit(0);
+                        break;
+                    
+                    default:
+                        try
+                        {
+                            var user = vkHandler.getUser(str);
+                            Console.WriteLine("Enter message:");
+                            str = Console.ReadLine();
+                            vkHandler.SendMessage(user, str);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        
+                        break;
+                }
             }
         }
 
--- SocialNetwork/SocialNetwork/VKHandler.cs
+++ SocialNetwork/SocialNetwork/VKHandler.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using VkNet;
 using VkNet.AudioBypassService.Extensions;
 using VkNet.Enums.Filters;
 using VkNet.Model;
+using VkNet.Model.RequestParams;
 using VkNet.Utils;
 
 namespace SocialNetwork
@@ -12,6 +15,7 @@ namespace SocialNetwork
         private const int AppId = 5288513;
         private readonly VkApi vk;
         private VkCollection<User> friends;
+        private readonly Random random;
 
         public VKHandler(string login, string password)
         {
@@ -26,6 +30,47 @@ namespace SocialNetwork
                 Password = password,
                 Settings = Settings.Friends
             });
+            
+            var filter = new FriendsGetParams {Fields = ProfileFields.FirstName | ProfileFields.LastName};
+            friends = vk.Friends.Get(filter);
+            
+            random = new Random();
+        }
+
+        public void WriteFriends()
+        {
+            var i = 1;
+            Console.WriteLine("Friends:");
+            foreach (var friend in friends)
+            {
+                Console.WriteLine(i + ")" + friend.FirstName + " " + friend.LastName);
+                ++i;
+            }
+        }
+
+        public User getUser(string fullName)
+        {
+            var split = fullName.Split();
+            if (split.Length != 2)
+            {
+                throw new ArgumentException("Incorrect user name");
+            }
+
+            var first = split[0];
+            var second = split[1];
+            return friends.First(f => (f.FirstName == first && f.LastName == second)
+                                      || (f.FirstName == second && f.LastName == first));
+        }
+
+        public void SendMessage(User user, string message)
+        {
+            var messageParams = new MessagesSendParams
+            {
+                Message = message,
+                UserId = user.Id,
+                RandomId = random.Next()
+            };
+            vk.Messages.Send(messageParams);
         }
     }
 }
