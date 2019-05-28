83a6f157f05730f7f5e830a4ab64794772279f74
Vlad264 <vlad-498@yandex.ru>
2019-05-28 10:16:27 +0700

    Create new migration

---
+++ EffectiveEmployee/EffectiveEmployee/Migrations/20190528031232_NewMig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EffectiveEmployee.Migrations
+{
+    public partial class NewMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Deadline",
+                table: "Projects",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Education",
+                table: "Employees",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deadline",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Education",
+                table: "Employees");
+        }
+    }
+}
